4a7a49976e7f2ae5d50891da67858c67be35ead3
ChenYizhenCN <63712821+ChenYizhenCN@users.noreply.github.com>
2020-04-27 21:22:26 +0800

    Add files via upload

---
+++ tools/sousuochaxun.js
@@ -0,0 +1,554 @@
+	
+function Di(){
+	var di1=document.getElementById("a1").innerHTML;
+	var di2=document.getElementById("a2").innerHTML;
+	var di3=document.getElementById("a3").innerHTML;
+	var di4=document.getElementById("a4").innerHTML;
+	var di5=document.getElementById("a5").innerHTML;
+	var di6=document.getElementById("a6").innerHTML;
+	var di7=document.getElementById("a7").innerHTML;
+	var di8=document.getElementById("a8").innerHTML;
+	var di9=document.getElementById("a9").innerHTML;
+	var di10=document.getElementById("a10").innerHTML;
+	var di11=document.getElementById("a11").innerHTML;
+	var di12=document.getElementById("a12").innerHTML;
+	var di13=document.getElementById("a13").innerHTML;
+	var di14=document.getElementById("a14").innerHTML;
+	var di15=document.getElementById("a15").innerHTML;
+	var di16=document.getElementById("a16").innerHTML;
+	var di17=document.getElementById("a17").innerHTML;
+	var di18=document.getElementById("a18").innerHTML;
+	var di19=document.getElementById("a19").innerHTML;
+	var di20=document.getElementById("a20").innerHTML;
+	var di21=document.getElementById("a21").innerHTML;
+	var di22=document.getElementById("a22").innerHTML;
+	var di23=document.getElementById("a23").innerHTML;
+	var di24=document.getElementById("a24").innerHTML;
+	var di25=document.getElementById("a25").innerHTML;
+	var di26=document.getElementById("a26").innerHTML;
+	var di27=document.getElementById("a27").innerHTML;
+	var di28=document.getElementById("a28").innerHTML;
+	var di29=document.getElementById("a29").innerHTML;
+	var di30=document.getElementById("a30").innerHTML;
+	var di31=document.getElementById("a31").innerHTML;
+	var di32=document.getElementById("a32").innerHTML;
+	var di33=document.getElementById("a33").innerHTML;
+	var di34=document.getElementById("a34").innerHTML;
+	var di35=document.getElementById("a35").innerHTML;
+	var di36=document.getElementById("a36").innerHTML;
+	var di37=document.getElementById("a37").innerHTML;
+	var di38=document.getElementById("a38").innerHTML;
+	var di39=document.getElementById("a39").innerHTML;
+	var di40=document.getElementById("a40").innerHTML;
+	var di41=document.getElementById("a41").innerHTML;
+	var di42=document.getElementById("a42").innerHTML;
+	var di43=document.getElementById("a43").innerHTML;
+	var di44=document.getElementById("a44").innerHTML;
+	var di45=document.getElementById("a45").innerHTML;
+	var di46=document.getElementById("a46").innerHTML;
+	var di47=document.getElementById("a47").innerHTML;
+	var di48=document.getElementById("a48").innerHTML;
+	var di49=document.getElementById("a49").innerHTML;
+	var di50=document.getElementById("a50").innerHTML;
+	var di51=document.getElementById("a51").innerHTML;
+	var di52=document.getElementById("a52").innerHTML;
+	var di53=document.getElementById("a53").innerHTML;
+	var di54=document.getElementById("a54").innerHTML;
+	var di55=document.getElementById("a55").innerHTML;
+	var di56=document.getElementById("a56").innerHTML;
+	var di57=document.getElementById("a57").innerHTML;
+	var di58=document.getElementById("a58").innerHTML;
+	var di59=document.getElementById("a59").innerHTML;
+	var di60=document.getElementById("a60").innerHTML;
+	var di61=document.getElementById("a61").innerHTML;
+	var di62=document.getElementById("a62").innerHTML;
+	var di63=document.getElementById("a63").innerHTML;
+	var di64=document.getElementById("a64").innerHTML;
+	var di65=document.getElementById("a65").innerHTML;
+	var di66=document.getElementById("a66").innerHTML;
+	var di67=document.getElementById("a67").innerHTML;
+	var di68=document.getElementById("a68").innerHTML;
+	var di69=document.getElementById("a69").innerHTML;
+	var di70=document.getElementById("a70").innerHTML;
+	var di71=document.getElementById("a71").innerHTML;
+	var di72=document.getElementById("a72").innerHTML;
+	var di73=document.getElementById("a73").innerHTML;
+	var di74=document.getElementById("a74").innerHTML;
+	var di75=document.getElementById("a75").innerHTML;
+	var di76=document.getElementById("a76").innerHTML;
+	var id=document.getElementById("id1").value;
+	if(id==di1){
+		d1=0.09;
+	document.getElementById("di1").value=d1;
+	}else if(id==di2){
+	    d2=0.18;
+	document.getElementById("di1").value=d2;
+	}else if(id==di3){
+	    d3=0.9;
+	document.getElementById("di1").value=d3;
+	}else if(id==di4){
+	    d4=1.25;
+	document.getElementById("di1").value=d4;
+	}else if(id==di5){
+	    d5=1.43;
+	document.getElementById("di1").value=d5;
+	}else if(id==di6){
+	    d6=1.696;
+	document.getElementById("di1").value=d6;
+	}else if(id==di7){
+	    d7=1.78;
+	document.getElementById("di1").value=d7;
+	}else if(id==di8){
+	    d8=2.14;
+	document.getElementById("di1").value=d8;
+	}else if(id==di9){
+	    d9=3.21;
+	document.getElementById("di1").value=d9;
+	}else if(id==di10){
+	    d10=3.74;
+	document.getElementById("di1").value=d10;
+	}else if(id==di11){
+	    d11=5.89;
+	document.getElementById("di1").value=d11;
+	}else if(id==di12){
+	    d12=9.73;
+	document.getElementById("di1").value=d12;
+	}else if(id==di13){
+	    d13=0.6;
+	document.getElementById("di1").value=d13;
+	}else if(id==di14){
+	    d14=2700;
+	document.getElementById("di1").value=d14;
+	}else if(id==di15){
+	    d15=8900;
+	document.getElementById("di1").value=d15;
+	}else if(id==di16){
+	    d16=8400;
+	document.getElementById("di1").value=d16;
+	}else if(id==di17){
+	    d17=7190;
+	document.getElementById("di1").value=d17;
+	}else if(id==di18){
+	    d18=7430;
+	document.getElementById("di1").value=d18;
+	}else if(id==di19){
+	    d19=5765;
+	document.getElementById("di1").value=d19;
+	}else if(id==di20){
+	    d20=0.7;
+	document.getElementById("di1").value=d20;
+	}else if(id==di21){
+	    d21=0.78;
+	document.getElementById("di1").value=d21;
+	}else if(id==di22){
+	    d22=1.17;
+	document.getElementById("di1").value=d22;
+	}else if(id==di23){
+	    d23=1.25;
+	document.getElementById("di1").value=d23;
+	}else if(id==di24){
+	    d24=1.29;
+	document.getElementById("di1").value=d24;
+	}else if(id==di25){
+	    d25=1.34;
+	document.getElementById("di1").value=d25;
+	}else if(id==di26){
+	    d26=1.54;
+	document.getElementById("di1").value=d26;
+	}else if(id==di27){
+	    d27=1.98;
+	document.getElementById("di1").value=d27;
+	}else if(id==di28){
+	    d28=1.98;
+	document.getElementById("di1").value=d28;
+	}else if(id==di29){
+	    d29=2.34;
+	document.getElementById("di1").value=d29;
+	}else if(id==di30){
+	    d30=2.93;
+	document.getElementById("di1").value=d30;
+	}else if(id==di31){
+	    d31=3.64;
+	document.getElementById("di1").value=d31;
+	}else if(id==di32){
+	    d32=5.79;
+	document.getElementById("di1").value=d32;
+	}else if(id==di33){
+	    d33=8900;
+	document.getElementById("di1").value=d33;
+	}else if(id==di34){
+	    d34=8930;
+	document.getElementById("di1").value=d34;
+	}else if(id==di35){
+	    d35=1738;
+	document.getElementById("di1").value=d35;
+	}else if(id==di36){
+	    d36=7133;
+	document.getElementById("di1").value=d36;
+	}else if(id==di37){
+	    d37=970;
+	document.getElementById("di1").value=d37;
+	}else if(id==di38){
+	    d38=1180;
+	document.getElementById("di1").value=d38;
+	}else if(id==di39){
+	    d39=700;
+	document.getElementById("di1").value=d39;
+	}else if(id==di40){
+	    d40=710;
+	document.getElementById("di1").value=d40;
+	}else if(id==di41){
+	    d41=760;
+	document.getElementById("di1").value=d41;
+	}else if(id==di42){
+	    d42=790;
+	document.getElementById("di1").value=d42;
+	}else if(id==di43){
+	    d43=800;
+	document.getElementById("di1").value=d43;
+	}else if(id==di44){
+	    d44=800;
+	document.getElementById("di1").value=d44;
+	}else if(id==di45){
+	    d45=860;
+	document.getElementById("di1").value=d45;
+	}else if(id==di46){
+	    d46=880;
+	document.getElementById("di1").value=d46;
+	}else if(id==di47){
+	    d47=1290;
+	document.getElementById("di1").value=d47;
+	}else if(id==di48){
+	    d48=1260;
+	document.getElementById("di1").value=d48;
+	}else if(id==di49){
+	    d49=920;
+	document.getElementById("di1").value=d49;
+	}else if(id==di50){
+	    d50=950;
+	document.getElementById("di1").value=d50;
+	}else if(id==di51){
+	    d51=970;
+	document.getElementById("di1").value=d51;
+	}else if(id==di52){
+	    d52=19302;
+	document.getElementById("di1").value=d52;
+	}else if(id==di53){
+	    d53=10490;
+	document.getElementById("di1").value=d53;
+	}else if(id==di54){
+	    d54=11340;
+	document.getElementById("di1").value=d54;
+	}else if(id==di55){
+	    d55=19254;
+	document.getElementById("di1").value=d55;	
+    }else if(id==di56){
+	    d56=16600;
+	document.getElementById("di1").value=d56;
+	}else if(id==di57){
+	    d57=1820;
+	document.getElementById("di1").value=d57;
+	}else if(id==di58){
+	    d58=1000;
+	document.getElementById("di1").value=d58;
+	}else if(id==di59){
+	    d59=1030;
+	document.getElementById("di1").value=d59;
+	}else if(id==di60){
+	    d60=1050;
+	document.getElementById("di1").value=d60;
+	}else if(id==di61){
+	    d61=1200;
+	document.getElementById("di1").value=d61;
+	}else if(id==di62){
+	    d62=930;
+	document.getElementById("di1").value=d62;
+	}else if(id==di63){
+	    d63=930;
+	document.getElementById("di1").value=d63;
+	}else if(id==di64){
+	    d64=1400;
+	document.getElementById("di1").value=d64;
+	}else if(id==di65){
+	    d65=1500;
+	document.getElementById("di1").value=d65;
+	}else if(id==di66){
+	    d66=1800;
+	document.getElementById("di1").value=d66;
+	}else if(id==di67){
+	    d67=3120;
+	document.getElementById("di1").value=d67;
+	}else if(id==di68){
+	    d68=14200;
+	document.getElementById("di1").value=d68;
+	}else if(id==di69){
+	    d69=920;
+	document.getElementById("di1").value=d69;
+	}else if(id==di70){
+	    d70=7800;
+	document.getElementById("di1").value=d70;
+	}else if(id==di71){
+	    d71=7850;
+	document.getElementById("di1").value=d71;
+	}else if(id==di72){
+	    d72=7200;
+	document.getElementById("di1").value=d72;
+	}else if(id==di73){
+	    d73=3000;
+	document.getElementById("di1").value=d73;
+	}else if(id==di74){
+	    d74=2300;
+	document.getElementById("di1").value=d74;
+	}else if(id==di75){
+	    d75=21450;
+	document.getElementById("di1").value=d75;
+	}else if(id==di76){
+	    d76=1500;
+	document.getElementById("di1").value=d76;
+	}else{
+		alert("您输入的介质表中不存在");
+		}
+	
+}
+
+
+
+
+
+
+
+function Bi(){
+	var bi1=document.getElementById("b1").innerHTML;
+	var bi2=document.getElementById("b2").innerHTML;
+	var bi3=document.getElementById("b3").innerHTML;
+	var bi4=document.getElementById("b4").innerHTML;
+	var bi5=document.getElementById("b5").innerHTML;
+	var bi6=document.getElementById("b6").innerHTML;
+	var bi7=document.getElementById("b7").innerHTML;
+	var bi8=document.getElementById("b8").innerHTML;
+	var bi9=document.getElementById("b9").innerHTML;
+	var bi10=document.getElementById("b10").innerHTML;
+	var bi11=document.getElementById("b11").innerHTML;
+	var bi12=document.getElementById("b12").innerHTML;
+	var bi13=document.getElementById("b13").innerHTML;
+	var bi14=document.getElementById("b14").innerHTML;
+	var bi15=document.getElementById("b15").innerHTML;
+	var bi16=document.getElementById("b16").innerHTML;
+	var bi17=document.getElementById("b17").innerHTML;
+	var bi18=document.getElementById("b18").innerHTML;
+	var bi19=document.getElementById("b19").innerHTML;
+	var bi20=document.getElementById("b20").innerHTML;
+	var bi21=document.getElementById("b21").innerHTML;
+	var bi22=document.getElementById("b22").innerHTML;
+	var bi23=document.getElementById("b23").innerHTML;
+	var bi24=document.getElementById("b24").innerHTML;
+	var bi25=document.getElementById("b25").innerHTML;
+	var bi26=document.getElementById("b26").innerHTML;
+	var bi27=document.getElementById("b27").innerHTML;
+	var bi28=document.getElementById("b28").innerHTML;
+	var bi29=document.getElementById("b29").innerHTML;
+	var bi30=document.getElementById("b30").innerHTML;
+	var bi31=document.getElementById("b31").innerHTML;
+	var bi32=document.getElementById("b32").innerHTML;
+	var bi33=document.getElementById("b33").innerHTML;
+	var bi34=document.getElementById("b34").innerHTML;
+	var bi35=document.getElementById("b35").innerHTML;
+	var bi36=document.getElementById("b36").innerHTML;
+	var bi37=document.getElementById("b37").innerHTML;
+	var bi38=document.getElementById("b38").innerHTML;
+	var bi39=document.getElementById("b39").innerHTML;
+	var bi40=document.getElementById("b40").innerHTML;
+	var bi41=document.getElementById("b41").innerHTML;
+	var bi42=document.getElementById("b42").innerHTML;
+	var bi43=document.getElementById("b43").innerHTML;
+	var bi44=document.getElementById("b44").innerHTML;
+	var bi45=document.getElementById("b45").innerHTML;
+	var bi46=document.getElementById("b46").innerHTML;
+	var bi47=document.getElementById("b47").innerHTML;
+	var bi48=document.getElementById("b48").innerHTML;
+	var bi49=document.getElementById("b49").innerHTML;
+	var bi50=document.getElementById("b50").innerHTML;
+	var bi51=document.getElementById("b51").innerHTML;
+	var bi52=document.getElementById("b52").innerHTML;
+	var bi53=document.getElementById("b53").innerHTML;
+	var bi54=document.getElementById("b54").innerHTML;
+	var bi55=document.getElementById("b55").innerHTML;
+	var bi56=document.getElementById("b56").innerHTML;
+	var bi57=document.getElementById("b57").innerHTML;
+	var ib=document.getElementById("ib1").value;
+	if(ib==bi1){
+		b1=3.41;
+	document.getElementById("bi1").value=b1;
+	}else if(ib==bi2){
+	    b2=0.24;
+	document.getElementById("bi1").value=b2;
+	}else if(ib==bi3){
+	    b3=0.44;
+	document.getElementById("bi1").value=b3;
+	}else if(ib==bi4){
+	    b4=0.34;
+	document.getElementById("bi1").value=b4;
+	}else if(ib==bi5){
+	    b5=0.24;
+	document.getElementById("bi1").value=b5;
+	}else if(ib==bi6){
+	    b6=0.52;
+	document.getElementById("bi1").value=b6;
+	}else if(ib==bi7){
+	    b7=0.2;
+	document.getElementById("bi1").value=b7;
+	}else if(ib==bi8){
+	    b8=0.6;
+	document.getElementById("bi1").value=b8;
+	}else if(ib==bi9){
+	    b9=0.25;
+	document.getElementById("bi1").value=b9;
+	}else if(ib==bi10){
+	    b10=1;
+	document.getElementById("bi1").value=b10;
+	}else if(ib==bi11){
+	    b11=0.58;
+	document.getElementById("bi1").value=b11;
+	}else if(ib==bi12){
+	    b12=0.58;
+	document.getElementById("bi1").value=b12;
+	}else if(ib==bi13){
+	    b13=0.56;
+	document.getElementById("bi1").value=b13;
+	}else if(ib==bi14){
+	    b14=0.51;
+	document.getElementById("bi1").value=b14;
+	}else if(ib==bi15){
+	    b15=0.4;
+	document.getElementById("bi1").value=b15;
+	}else if(ib==bi16){
+	    b16=0.5;
+	document.getElementById("bi1").value=b16;
+	}else if(ib==bi17){
+	    b17=0.42;
+	document.getElementById("bi1").value=b17;
+	}else if(ib==bi18){
+	    b18=0.4;
+	document.getElementById("bi1").value=b18;
+	}else if(ib==bi19){
+	    b19=0.6;
+	document.getElementById("bi1").value=b19;
+	}else if(ib==bi20){
+	    b20=0.55;
+	document.getElementById("bi1").value=b20;
+	}else if(ib==bi21){
+	    b21=0.67;
+	document.getElementById("bi1").value=b21;
+	}else if(ib==bi22){
+	    b22=0.69;
+	document.getElementById("bi1").value=b22;
+	}else if(ib==bi23){
+	    b23=0.51;
+	document.getElementById("bi1").value=b23;
+	}else if(ib==bi24){
+	    b24=0.52;
+	document.getElementById("bi1").value=b24;
+	}else if(ib==bi25){
+	    b25=0.42;
+	document.getElementById("bi1").value=b25;
+	}else if(ib==bi26){
+	    b26=0.24;
+	document.getElementById("bi1").value=b26;
+	}else if(ib==bi27){
+	    b27=0.34;
+	document.getElementById("bi1").value=b27;
+	}else if(ib==bi28){
+	    b28=0.47;
+	document.getElementById("bi1").value=b28;
+	}else if(ib==bi29){
+	    b29=0.43;
+	document.getElementById("bi1").value=b29;
+	}else if(ib==bi30){
+	    b30=0.46;
+	document.getElementById("bi1").value=b30;
+	}else if(ib==bi31){
+	    b31=0.18;
+	document.getElementById("bi1").value=b31;
+	}else if(ib==bi32){
+	    b32=0.5;
+	document.getElementById("bi1").value=b32;
+	}else if(ib==bi33){
+	    b33=0.77;
+	document.getElementById("bi1").value=b33;
+	}else if(ib==bi34){
+	    b34=0.82;
+	document.getElementById("bi1").value=b34;
+	}else if(ib==bi35){
+	    b35=0.19;
+	document.getElementById("bi1").value=b35;
+	}else if(ib==bi36){
+	    b36=0.21;
+	document.getElementById("bi1").value=b36;
+	}else if(ib==bi37){
+	    b37=0.49;
+	document.getElementById("bi1").value=b37;
+	}else if(ib==bi38){
+	    b38=0.4;
+	document.getElementById("bi1").value=b38;
+	}else if(ib==bi39){
+	    b39=0.34;
+	document.getElementById("bi1").value=b39;
+	}else if(ib==bi40){
+	    b40=0.35;
+	document.getElementById("bi1").value=b40;
+	}else if(ib==bi41){
+	    b41=0.26;
+	document.getElementById("bi1").value=b41;
+	}else if(ib==bi42){
+	    b42=0.22;
+	document.getElementById("bi1").value=b42;
+	}else if(ib==bi43){
+	    b43=0.17;
+	document.getElementById("bi1").value=b43;
+	}else if(ib==bi44){
+	    b44=0.18;
+	document.getElementById("bi1").value=b44;
+	}else if(ib==bi45){
+	    b45=0.174;
+	document.getElementById("bi1").value=b45;
+	}else if(ib==bi46){
+	    b46=0.2;
+	document.getElementById("bi1").value=b46;
+	}else if(ib==bi47){
+	    b47=0.13;
+	document.getElementById("bi1").value=b47;
+	}else if(ib==bi48){
+	    b48=0.09;
+	document.getElementById("bi1").value=b48;
+	}else if(ib==bi49){
+	    b49=0.056;
+	document.getElementById("bi1").value=b49;
+	}else if(ib==bi50){
+	    b50=0.051;
+	document.getElementById("bi1").value=b50;
+	}else if(ib==bi51){
+	    b51=0.09;
+	document.getElementById("bi1").value=b51;
+	}else if(ib==bi52){
+	    b52=0.106;
+	document.getElementById("bi1").value=b52;
+	}else if(ib==bi53){
+	    b53=0.12;
+	document.getElementById("bi1").value=b53;
+	}else if(ib==bi54){
+	    b54=0.118;
+	document.getElementById("bi1").value=b54;
+	}else if(ib==bi55){
+	    b55=0.092;
+	document.getElementById("bi1").value=b55;	
+    }else if(ib==bi56){
+	    b56=0.033;
+	document.getElementById("bi1").value=b56;
+	}else if(ib==bi57){
+	    b57=0.215;
+	document.getElementById("bi1").value=b57;
+	}else{
+		alert("您输入的介质表中不存在");
+		}
+	
+}
